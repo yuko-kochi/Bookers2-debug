836205b4370af9522544a36cf11eead8268e9283
yuko-kochi <happy.k165131@gmail.com>
2021-07-01 12:36:21 +0000

    [Add]Favorites

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
 
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books
 
+  resources :books do
+   resource :favorites, only: [:create, :destroy]
+  end
 
 end
