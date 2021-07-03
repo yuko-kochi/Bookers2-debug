a0f573ff69de8692855dc7ab4552a0c2366c38fc
yuko-kochi <happy.k165131@gmail.com>
2021-07-03 04:05:16 +0000

    [Add]comment

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,8 +4,11 @@ class BookCommentsController < ApplicationController
     book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    if comment.save
+      redirect_to book_path(book)
+    else
+      redirect_to book_path(book)
+    end
   end
 
   def destroy
