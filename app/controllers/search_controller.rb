3ff0d7a800d7460142b69b2d39a64892a77bc13d
yuko-kochi <happy.k165131@gmail.com>
2021-07-03 09:27:51 +0000

    [Add]search

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,27 @@
+class SearchController < ApplicationController
+
+  def search
+    @model = params["model"]
+    @content = params["content"]
+    @method = params["method"]
+    @datas = search_for(@model, @content, @method)
+  end
+
+  private
+  def search_for(model, content, method)
+    if model == 'user'
+      if method == 'perfect'
+        User.where(name: content)
+      else
+        User.where('name LIKE ?', '%'+content+'%')
+      end
+    elsif model == 'book'
+      if method == 'perfect'
+        Book.where(title: content)
+      else
+        Book.where('title LIKE ?', '%'+content+'%')
+      end
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,13 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
   get 'home/about' => 'homes#about'
+  get '/search' => 'search#search'
 
-resources :users, only: [:index, :show, :edit, :update] do
-  resource :relationships, only: [:create, :destroy]
-  get 'followings' => 'relationships#followings', as: 'followings'
-  get 'followers' => 'relationships#followers', as: 'followers'
-end
+  resources :users, only: [:index, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
 
   resources :books do
     resource :favorites, only: [:create, :destroy]
